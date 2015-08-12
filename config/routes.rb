3cf842f5f9151b5a459f92981caf75d72134b756
Zach Whelchel <zach.whelchel@gmail.com>
2015-08-12 12:00:11 -0400

    started new blog post

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,7 @@ Rails.application.routes.draw do
   get 'posts/every_submitted_app_idea', :to => 'posts#every_submitted_app_idea'
   get 'posts/calling_all_app_makers', :to => 'posts#calling_all_app_makers'
   get 'posts/unique_gameplay_got_carl_rejected_from_the_app_store', :to => 'posts#unique_gameplay_got_carl_rejected_from_the_app_store'
+  get 'posts/creating_roll_out', :to => 'posts#creating_roll_out'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
