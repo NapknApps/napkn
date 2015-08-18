c7308d1cbc379a89b6412c36ea7075e0251ba989
Zach Whelchel <zach.whelchel@gmail.com>
2015-08-17 21:40:50 -0400

    Various changes to blog.

--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,7 @@ Rails.application.routes.draw do
   get 'posts/calling_all_app_makers', :to => 'posts#calling_all_app_makers'
   get 'posts/unique_gameplay_got_carl_rejected_from_the_app_store', :to => 'posts#unique_gameplay_got_carl_rejected_from_the_app_store'
   get 'posts/creating_roll_out', :to => 'posts#creating_roll_out'
+  get 'posts/creating_nomojo', :to => 'posts#creating_nomojo'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
